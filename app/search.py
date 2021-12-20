575dbc6e6fd011b17eebddd6f7381cc7b4e2dc5a
e.gerasin <e.gerasin@bars.group>
2021-12-20 15:00:52 +0300

    Минимальный набор функций готов

--- app/search.py
+++ app/search.py
@@ -1,34 +1,29 @@
-# import csv
-# from typing import List
-# import requests
-# from lxml import html
+import requests
+from lxml import html
 
-# from entities import Link
+from entities import Link, Links
 
 
-# class Searcher:
+class Searcher:
 
-#     def __init__(self, url: str) -> None:
-#         self._url = url
+    def __init__(self, url: str) -> None:
+        """Initialization of the link finder object
+        Args:
+            url (str): url where find links
+        """
+        self._url = url
 
-#     def run(self) -> List[Link]:
+    def run(self) -> Links:
+        """Start search
 
-#         links = list()
-#         page = requests.get(self._url).content
-#         page_html = html.fromstring(page)
-#         result = page_html.xpath('//a[@href]/@href')
-#         for link in result:
-#             if 'https' in link or 'http' in link:
-#                 value = Link(link)
-#                 links.append(value)
-#         return links
-
-# class Keeper:
-
-#     def __init__(self) -> None:
-        
-#         self._name_file = ""
-
-#     def save(self, name_file, path_to_save):
-
-        
+        Returns:
+            Links: object with found links
+        """
+        list_links = Links()
+        page = requests.get(self._url).content
+        page_html = html.fromstring(page)
+        result = page_html.xpath('//a[@href]/@href')
+        for link in result:
+            if 'https' in link or 'http' in link:
+                list_links.add(Link(link))
+        return list_links
